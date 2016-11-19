882a94183fe20f58d9979342d69ae9894d66b07f
Christopher Van <cvan@mozilla.com>
2016-11-18 18:00:13 -0800

    THIS IS NOW

---
+++ main.js
@@ -0,0 +1,9 @@
+/* jshint ignore:start */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){  // eslint-disable-line
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),    // eslint-disable-line
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)  // eslint-disable-line
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');  // eslint-disable-line
+/* jshint ignore:end */
+
+ga('create', '', 'auto');
+ga('send', 'pageview');
